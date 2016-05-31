bde637c2701917d84c7f71a972bb19b7b7e5a90c
Darius Wattimena <dariuswattimena@hotmail.com>
2016-05-31 22:10:06 +0200

    Commit

--- image.js
+++
@@ -1,9 +0,0 @@
-
-
-function showLetter() {
-    var name = imagesArray[generateRandomForArray()];
-    $("div").append("<img src='images/" + name + ".png'>");
-    var left = generateRandom();
-    var top = generateRandom();
-    $("div").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
-}
