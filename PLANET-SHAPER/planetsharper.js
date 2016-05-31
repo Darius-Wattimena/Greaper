eef8beb3871769094c784a2d63093fdb626f1487
Darius Wattimena <dariuswattimena@hotmail.com>
2016-05-31 21:35:47 +0200

    Commit

--- PLANET-SHAPER/planetsharper.js
+++ PLANET-SHAPER/planetsharper.js
@@ -8,3 +8,22 @@ var sound = new Howl({
 function run() {
 	sound.play();
 }
+
+var x;
+
+function changecolors() {
+    x = 1;
+    setInterval(change, 1000);
+}
+
+function change() {
+    if (x === 1) {
+        color = "red";
+        x = 2;
+    } else {
+        color = "green";
+        x = 1;
+    }
+
+    document.body.style.background = color;
+}
