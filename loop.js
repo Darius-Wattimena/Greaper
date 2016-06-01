56465aad668a446d297e870c78b8079b69fdfe70
Darius Wattimena <dariuswattimena@hotmail.com>
2016-06-01 18:45:49 +0200

    Commit

--- loop.js
+++ loop.js
@@ -1,5 +1,5 @@
 var sound = new Howl({
-  urls: ['dl.dropbox.com/s/62l4s67x6gd81jg/loop.mp3'],
+  urls: ['https://dl.dropbox.com/s/62l4s67x6gd81jg/loop.mp3'],
   html5: true,
   autoplay: true,
   loop: true,
